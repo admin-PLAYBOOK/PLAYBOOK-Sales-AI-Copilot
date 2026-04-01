1047f609ad3911c3d15f59ffa3db70dfbabdabfc
alya <admin@get-playbook.com>
2026-04-01 13:20:50 +0300

    Database connected, now needs to actually display in admin panel

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false, // Required for Neon
+        rejectUnauthorized: false, 
     },
     connectionTimeoutMillis: 5000,
     idleTimeoutMillis: 30000,
-    max: 10,
+    max: 20,
 });
 
 // Test connection
